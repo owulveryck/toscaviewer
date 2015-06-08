4bfb20c5b48073dd3c3a237dee15a66b02b51e7e
Olivier W <olivier.wulveryck@gmail.com>
2015-06-08 23:09:35 +0200

    Version KO

--- test.go
+++ test.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"log"
 
+	"github.com/gonum/matrix/mat64"
 	"github.com/owulveryck/toscalib"
 	"gopkg.in/yaml.v2"
 )
@@ -22,8 +23,26 @@ func main() {
 	// Here we have the structure
 	// Count the nodes
 
-	log.Printf("Found %v nodes\n", len(mystruct.TopologyTemplate.NodeTemplates))
+	//log.Printf("Found %v nodes\n", len(mystruct.TopologyTemplate.NodeTemplates))
+	nodeNum := len(mystruct.TopologyTemplate.NodeTemplates)
+	i := 0
+	nodeReferences := make(map[string]int, nodeNum)
 	for nodeName, nodeDetail := range mystruct.TopologyTemplate.NodeTemplates {
-		log.Printf(" Node %v = %v", nodeName, nodeDetail.Type)
+		//log.Printf(" Node %v = %v", nodeName, nodeDetail.Type)
+		nodeReferences[nodeName] = i
+		i = i + 1
+		log.Println(nodeDetail.Requirements)
 	}
+	// Create an adjacency Matrix
+	adjacencyMatrix := mat64.NewDense(nodeNum, nodeNum, nil)
+	for nodeName, nodeDetail := range mystruct.TopologyTemplate.NodeTemplates {
+		// If requirements are found
+		if nodeDetail.Requirements != nil {
+			for _, reqDetail := range nodeDetail.Requirements {
+				// Set 1 to the adjacencymatrix
+				adjacencyMatrix.Set(nodeReferences[nodeName], nodeReferences[reqDetail["host"]], 1)
+			}
+		}
+	}
+
 }
