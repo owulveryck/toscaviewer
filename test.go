cedb206e36aa8371a37655644bf3e6c881b95e7b
Olivier Wulveryck <olivier.wulveryck-ext@socgen.com>
2015-06-09 12:31:51 +0200

    Version generating dot file only for debugging purpose

--- test.go
+++ test.go
@@ -4,9 +4,10 @@ import (
 	"io/ioutil"
 	"log"
 
-	"github.com/gonum/matrix/mat64"
+	//"github.com/gonum/matrix/mat64"
 	"github.com/owulveryck/toscalib"
 	"gopkg.in/yaml.v2"
+	"fmt"
 )
 
 func main() {
@@ -27,22 +28,30 @@ func main() {
 	nodeNum := len(mystruct.TopologyTemplate.NodeTemplates)
 	i := 0
 	nodeReferences := make(map[string]int, nodeNum)
-	for nodeName, nodeDetail := range mystruct.TopologyTemplate.NodeTemplates {
+	for nodeName, _ := range mystruct.TopologyTemplate.NodeTemplates {
 		//log.Printf(" Node %v = %v", nodeName, nodeDetail.Type)
 		nodeReferences[nodeName] = i
 		i = i + 1
-		log.Println(nodeDetail.Requirements)
 	}
 	// Create an adjacency Matrix
-	adjacencyMatrix := mat64.NewDense(nodeNum, nodeNum, nil)
+	//adjacencyMatrix := mat64.NewDense(nodeNum, nodeNum, nil)
+	//2015/06/09 11:41:51 (mysql_database) reqDetail : [map[host:map[node:mysql_dbms]]]
+	//2015/06/09 11:41:51 (mysql_dbms) reqDetail : [map[host:map[node:server]]]
+	//2015/06/09 11:41:51 (webserver) reqDetail : [map[host:map[node:server]]]
+	//2015/06/09 11:41:51 (wordpress) reqDetail : [map[host:map[node:webserver]] map[database_endpoint:map[node:mysql_database]]]
+
+	fmt.Printf("digraph G {\n")
 	for nodeName, nodeDetail := range mystruct.TopologyTemplate.NodeTemplates {
 		// If requirements are found
 		if nodeDetail.Requirements != nil {
-			for _, reqDetail := range nodeDetail.Requirements {
-				// Set 1 to the adjacencymatrix
-				adjacencyMatrix.Set(nodeReferences[nodeName], nodeReferences[reqDetail["host"]], 1)
+			log.Printf("(%v) reqDetail : %v",nodeName, nodeDetail.Requirements)
+			for _, requirementType := range nodeDetail.Requirements {
+				for requirementTypeProp, value := range requirementType {
+					fmt.Printf("\t%v -> %v [label = %v];\n",value["node"], nodeName, requirementTypeProp)
+				}
+				//adjacencyMatrix.Set(nodeReferences[nodeName],nodeReferences[target],1)
 			}
 		}
 	}
-
+	fmt.Printf("}\n")
 }
