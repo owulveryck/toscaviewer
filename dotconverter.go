fc10dd8fde4868b408b2ed4f4ad8455b19543a18
Olivier Wulveryck <olivier.wulveryck-ext@socgen.com>
2015-06-11 10:40:11 +0200

    Code is ok according to the implementation of the NodeTemplate in the toscalib

--- dotconverter.go
+++ dotconverter.go
@@ -7,6 +7,7 @@ import (
 	"github.com/owulveryck/toscalib"
 	"io"
 )
+
 // PrintDot convert the ToscaDefinitionure in dot format
 // in order to generate a graph with graphviz
 // This function is mostly used for debugging purpose and may change a lot in the future
@@ -15,14 +16,15 @@ func PrintDot(w io.Writer, topology toscalib.ToscaDefinition) {
 	fmt.Fprintf(w, "\tgraph [ rankdir = \"LR\" ];\n")
 	for nodeName, nodeDetail := range topology.TopologyTemplate.NodeTemplates {
 		// If requirements are found
-		fmt.Fprintf(w,"\t\"%v\" [ label = \"%v\" shape = circle color=blue]\n",nodeName,nodeName)
-		fmt.Fprintf(w,"\t\"%v\" [ label = \"%v\" shape = record color=red]\n",nodeDetail.Type,nodeDetail.Type)
-		fmt.Fprintf(w,"\t\"%v\" -> \"%v\" [ color = red ]\n",nodeDetail.Type,nodeName)
+		fmt.Fprintf(w, "\t\"%v\" [ label = \"%v\" shape = circle color=blue]\n", nodeName, nodeName)
+		fmt.Fprintf(w, "\t\"%v\" [ label = \"%v\" shape = record color=red]\n", nodeDetail.Type, nodeDetail.Type)
+		fmt.Fprintf(w, "\t\"%v\" -> \"%v\" [ color = red ]\n", nodeDetail.Type, nodeName)
 		if nodeDetail.Requirements != nil {
-			for _, requirementType := range nodeDetail.Requirements {
-				for requirementTypeProp, value := range requirementType {
-					fmt.Fprintf(w, "\t%v -> %v [label = %v];\n", nodeName, value["node"], requirementTypeProp)
-				}
+		        fmt.Printf("DEBUG: %v\n",nodeDetail.Requirements)
+			for _, requirementAssignements := range nodeDetail.Requirements {
+			    for requirementName , requirementAssignement := range requirementAssignements {
+				fmt.Fprintf(w, "\t%v -> %v [label = %v];\n", nodeName, requirementAssignement.Node, requirementName)
+			    }
 			}
 		}
 	}
